5038dbd7fdd8e5b5b9f638d343191a1c1ef640ef
ZakirovaVera <zakirovavk@gmail.com>
2022-11-29 00:15:49 +0500

    задача 1

--- task1.java
+++ task1.java
@@ -2,8 +2,35 @@
 // используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки. 
 // Если значение null, то параметр не должен попадать в запрос. 
 // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
-// Результат "select * from students where “name” = “Ivanov” and “country”=”Russia” …
+// Результат "select * from students where “name” = “Ivanov” and “country”=”Russia” and "city"="Moscow"
+
+import org.json.simple.JSONObject;
 
 public class task1 {
-    
+    public static void main(String[] args) {
+        String text = "select * from students where ";
+        JSONObject jo = new JSONObject();
+        jo.put("name", "Ivanov");
+        jo.put("country", "Russia");
+        jo.put("city", "Moscow");
+        jo.put("age", null);
+
+
+        System.out.println(jo.toJSONString());
+        name(jo, text);
+    }
+    public static void name(JSONObject jo, String sql) {
+        StringBuilder a = new StringBuilder(sql);
+        AddKey(jo, a, "name", false);
+        AddKey(jo, a, "country", true);
+        AddKey(jo, a, "city", true);
+        AddKey(jo, a, "age", true);
+        System.out.println(a);
+    }
+    public static void AddKey(JSONObject jo, StringBuilder sb, String key, Boolean isAddAnd) {
+        var keyValue = jo.get(key);
+        if(keyValue == null) return;
+        if(isAddAnd) sb.append(" and ");
+        sb.append(String.format("\"%s\" = \"%s\"", key, keyValue));
+    }
 }
